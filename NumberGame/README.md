9bd92259e4bc4d40db254295f2890375fa4993bb
Bucha1958 <okoriemmadu@gmail.com>
2023-12-11 12:25:24 +0100

    Refactor code to make it maintainable

--- NumberGame/README.md
+++ NumberGame/README.md
@@ -1,2 +1,64 @@
-# NumberGame with Java
-Implement a number game with Java
+# Guessing Game
+
+Welcome to the Guessing Game, a simple Java console application where players try to guess a randomly generated number.
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [How to Play](#how-to-play)
+- [Testing](#testing)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- **User Interaction:** Engage with players by prompting them to enter their name and guess a number.
+- **Random Number Generation:** Generates a random number for players to guess.
+- **Hints and Attempts:** Provides hints and limits the number of attempts to make the game challenging.
+- **Scoring:** Keeps track of the player's score based on successful guesses.
+- **Play Again:** Allows players to play multiple rounds without restarting the application.
+
+## Getting Started
+
+### Prerequisites
+
+- Java Development Kit (JDK) installed on your machine.
+- A Java IDE (Integrated Development Environment) or a text editor.
+
+### Installation
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/Bucha1958/CODSOFT.git
+
+
+## How to Play
+
+- Launch the application.
+- Enter your name when prompted.
+- Try to guess the randomly generated number within the given attempts.
+- Receive hints and feedback after each guess.
+- After a game, choose to play again or exit.
+
+## Testing
+
+- Still working on the test cases
+
+## Contributing
+
+- If you'd like to contribute or help with some implemetation to the project i will be happy for your contribution, please follow these steps:
+
+- Fork the project.
+- Create a new branch (git checkout -b feature/your-feature-name).
+- Commit your changes (git commit -am 'Add new feature').
+- Push to the branch (git push origin feature/your-feature-name).
+- Open a pull request.
+
+## License
+
+
+
--- NumberGame/RunMYGame.java
+++ NumberGame/RunMYGame.java
@@ -1,72 +1,138 @@
+import java.util.InputMismatchException;
 import java.util.Random;
-import java.util.*;
+import java.util.Scanner;
 
 class RunMyGame {
+
+    private static final Scanner scn = new Scanner(System.in);
+    private static String userName = "";
+    private static int score = 0;
+    private static int randomNumber;
+
+    private static final int MAX_ATTEMPTS = 5;
+    private static final int HINT_RANGE = 10;
+
     public static void main(String[] args) {
+        System.out.println("I am here to Entertain you");
+        System.out.println();
 
-        // Initialize random number generator and scanner
-        Random random = new Random();
-        Scanner scn = new Scanner(System.in);
-
-        // Generate a random number between 1 and 100 (inclusive)
-        int randomNumber = random.nextInt(100) + 1;
-        System.out.println(randomNumber);
+        // Get user information (name) and set up the initial game
+        getUserInfo();
 
         // Start an infinite loop for playing the game
         while (true) {
+            // Play the game
+            playGame();
+
             // Prompt the user to play again or exit
             System.out.print("Play again? Enter 'y' to continue or 'n' to exit: ");
-            
-           
-            char enterYesOrNo = '\u0000';
+            char enterYesOrNo = getUserInputChar();
+
+            // Check user's choice to continue playing or exit
+            if (enterYesOrNo == 'n') {
+                break;
+            }
+        }
+
+        // Close the Scanner when it's no longer needed
+        scn.close();
+    }
+
+    // Method to get user information (name) and set up the initial game
+    private static void getUserInfo() {
+        if (userName.trim().isEmpty()) {
+            System.out.println("May I know your name: ");
+            userName = scn.nextLine().trim();
+
+            if (userName.isEmpty()) {
+                System.out.println("Please provide a valid name.");
+                System.exit(0);
+            } else {
+                System.out.println(userName + " welcome to Guessing Game");
+                System.out.println();
+            }
+        }
+    }
+
+    // Method to handle the game logic
+    private static void playGame() {
+        // Initialize random number generator
+        Random random = new Random();
+        // Generate a random number between 1 and 100 (inclusive)
+        randomNumber = random.nextInt(100) + 1;
+        System.out.println(randomNumber);
+
+        // Loop through the attempts
+        for (int attempts = MAX_ATTEMPTS; attempts > 0; attempts--) {
+            // Display information about the attempts
+            displayAttemptsMessage(attempts);
+
+            // Get the user's guess
+            int guessNumber = getUserGuess();
+
+            // Compare the user's guess with the random number
+            if (randomNumber < guessNumber) {
+                System.out.println("Too high");
+            } else if (randomNumber > guessNumber) {
+                System.out.println("Too low");
+            } else {
+                // User guessed correctly
+                score++;
+                System.out.println("You guessed it correctly! " + userName + ", your score is " + score + " Keep it up!");
+                return; // Exit the method after a correct guess
+            }
+        }
+        // Out of attempts, display the correct number
+        System.out.println("Out of attempts. The correct number was: " + randomNumber);
+    }
+
+    // Method to display messages about the attempts
+    private static void displayAttemptsMessage(int attempts) {
+        if (attempts == MAX_ATTEMPTS) {
+            System.out.println("You have maximum " + attempts + " attempts");
+        } else if (attempts == 3) {
+            System.out.println(attempts + " attempts");
+        } else if (attempts == 2) {
+            // Provide a hint for the range of the correct number
+            int lowerbound = randomNumber - HINT_RANGE;
+            int upperbound = randomNumber + 5;
+            System.out.println(attempts + " attempts but I will give you a hint: the target number is between " + lowerbound + " and " + upperbound);
+        } else if (attempts == 1) {
+            System.out.println("Final attempt");
+        }
+    }
+
+    // Method to get the user's guess, handling invalid input
+    private static int getUserGuess() {
+        while (true) {
+            System.out.print("Enter a Number: ");
+
             try {
-                String userInput = scn.nextLine().trim(); // Trim to remove leading/trailing whitespaces
+                int guessNumber = scn.nextInt();
+                scn.nextLine(); // Consume the newline character
+                return guessNumber;
+            } catch (InputMismatchException ex) {
+                // Handle invalid input (non-integer)
+                System.out.println("Invalid input. Please enter a valid number.");
+                scn.nextLine(); // Consume the entire line to discard the invalid input
+            }
+        }
+    }
+
+    // Method to get a single character input from the user, handling invalid input
+    private static char getUserInputChar() {
+        while (true) {
+            try {
+                String userInput = scn.nextLine().trim();
                 if (userInput.length() == 1 && (userInput.charAt(0) == 'y' || userInput.charAt(0) == 'n')) {
-                    enterYesOrNo = userInput.charAt(0);
+                    return userInput.charAt(0);
                 } else {
                     throw new InputMismatchException("Invalid input");
                 }
             } catch (InputMismatchException e) {
-                System.out.print("Invalid input. Please enter 'y' or 'n'");
-                scn.nextLine(); // Consume the entire line to discard the invalid input
-                continue;
-            }
-            
-            if (enterYesOrNo == 'y') {
-                // Allow five attempts
-                for (int i = 5; i > 0; i--) {
-                    if (i == 5) {
-                        System.out.println("You have maximum " + i + " attempts");
-                    } else if (i < 5 && i > 1) {
-                        System.out.println(i + " attempts");
-                    } else {
-                        System.out.println("Final attempt");
-                    }
-
-                    System.out.print("Enter a Number: ");
-
-                    int guessNumber;
-                    try {
-                        guessNumber = scn.nextInt();
-                    } catch (Exception ex) {
-                        System.out.println("Invalid input. Please enter a valid number.");
-                        scn.nextLine(); // Consume the entire line to discard the invalid input
-                        i++; // Increment i to account for invalid attempt
-                        continue; // Continue the loop to prompt the user again
-                    }
-
-                    // Compare the user's guess with the random number
-                    if (randomNumber < guessNumber) {
-                        System.out.println("Too high");
-                    } else if (randomNumber > guessNumber) {
-                        System.out.println("Too low");
-                    } else {
-                        System.out.println("You guessed it correctly! Keep it up!");
-                        return;
-                    }
-                }
-            } else {
-                return;
+                // Handle invalid input (not 'y' or 'n')
+                System.out.println("Invalid input.");
+                System.out.println();
             }
         }
     }
